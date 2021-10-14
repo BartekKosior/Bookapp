4cdcf97702a607fd73fd80b357c10301d44ea5fc
Bartek Kosior <bartlomiejkosior2@gmail.com>
2021-10-14 22:10:29 +0200

    Task 10,2

--- src/js/script.js
+++ src/js/script.js
@@ -17,24 +17,22 @@
     constructor(){
       const thisBookList = this;
       thisBookList.getElements();
+      thisBookList.initData();
       thisBookList.render();
       thisBookList.initActions();
-      thisBookList.filterbooks();
-      thisBookList.determineRatingBgc(rating);
-
-
+      thisBookList.filterBooks();
     }
 
     initData(){
       this.data = dataSource.books;
+      this.filters = [];        // tablica dostepna we wszystkich metodach
     }
 
     getElements(){
       const thisBookList = this;
-      thisBookList.booklist = thisBookList.document.querySelector('.books-list');
-      thisBookList.form = thisBookList.document.querySelector('form');
-      thisBookList.bookImage = thisBookList.document.querySelector('book__image[data-id=id]');
-    
+      thisBookList.booklist = document.querySelector('.books-list');
+      thisBookList.form = document.querySelector('form');
+      // thisBookList.bookImage = document.querySelector('book__image[data-id=id]');
     }
 
     // eslint-disable-next-line no-unused-vars
@@ -42,19 +40,21 @@
       const thisBookList = this;
       // const booklist = document.querySelector('.books-list');
       for (let book of this.data  /*dataSource.books*/ ){
+        book.ratingBgc = this.determineRatingBgc(book.rating);
+        book.ratingWidth = book.rating*10;
         const generatedHTML = templates.booksList(book);
         // eslint-disable-next-line no-undef
         const element = utils.createDOMFromHTML(generatedHTML);
         thisBookList.booklist.appendChild(element);   //appendChild wykonuje się na alemencie html, nie na stringu
-        const ratingBgc = determineRatingBgc(rating); //stała która będzie równa temu, co zwróci determineRatingBgc dla rating danej książki.
-        const ratingWidth = ratingBgc*10         //stała która ustali długość paska. Musi się ona opierać na wartości ratingu. Jeśli rating równa się 5, to ratingWidth musi równać się 50 itd. Czyli tak naprawdę konwertujemy rating do skali procentowej.
+        // const ratingBgc = this.determineRatingBgc(book.rating); //stała która będzie równa temu, co zwróci determineRatingBgc dla rating danej książki.
+        // const ratingWidth = book.rating*10;         //stała która ustali długość paska. Musi się ona opierać na wartości ratingu. Jeśli rating równa się 5, to ratingWidth musi równać się 50 itd. Czyli tak naprawdę konwertujemy rating do skali procentowej.
       }
     }
 
     initActions(){
       const thisBookList = this;
       const favoriteBooks = [];
-      const filters = [];                                                     // musi być zdefiniowana w kazdej metodzie ???????????????
+      
       // ref do wszystkich elementów .book-image w liście .bookslist
       // const bookImages = document.querySelectorAll('.book__image');
       /* for(let bookImage of bookImages){
@@ -98,57 +98,53 @@
         if(event.target.tagName == 'INPUT' &&  event.target.type == 'checkbox' && event.target.name == 'filter'){
           console.log('event.target.value:', event.target.value);
           if(event.target.checked){
-            filters.push(event.target.value);
+            thisBookList.filters.push(event.target.value);
           } else {
-            const index = filters.indexOf(event.target.value);
-            filters.splice(index, 1);
-          }           
+            const index = thisBookList.filters.indexOf(event.target.value);
+            thisBookList.filters.splice(index, 1);
+          } 
+          thisBookList.filterBooks();
+          
         }
       });   
     }
 
     filterBooks(){
       const thisBookList = this;
-      const filters = [];
       for(let book of dataSource.books){
         let shouldBeHidden = false;
-        for(let filter of filters){
-          if(!book.details[filter]) { 
+        for(let filter of thisBookList.filters){
+          if(!book.details[filter]){ 
             shouldBeHidden = true;
             break;
           }
         }
 
+        const bookImage = document.querySelector('.book__image[data-id= "' +book.id+ '"]');
         if(shouldBeHidden == true){
-          //const bookImage = document.querySelectorAll('book__image[data-id="id"]');
-          thisBookList.bookImage.classList.add('hidden');
+          bookImage.classList.add('hidden');
         } else {
-          //const bookImage = document.querySelectorAll('book__image[data-id="id"]');
-          thisBookList.bookImage.classList.remove('hidden');
+          bookImage.classList.remove('hidden');
         }
       } 
     }
 
-    determineRatingBgc(rating) {
-      /*
-      Rating < 6
-      background: linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%);
-      
-      Rating > 6 && <= 8
-      background: linear-gradient(to bottom, #b4df5b 0%, #b4df5b 100%);
-      
-      Rating > 8 && <= 9
-      background: linear-gradient(to bottom, #299a0b 0%, #299a0b 100%);
-      
-      Rating > 9
-      background: linear-gradient(to bottom, #ff0084 0%,#ff0084 100%);
-      */
-
+    determineRatingBgc(rating){
       if(rating<6){
-        background: linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%);
+        return 'background: linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
       } 
       
-      
+      if(rating > 6 && rating <= 8){
+        return 'background: linear-gradient(to bottom, #b4df5b 0%, #b4df5b 100%)';
+      }
+
+      if(rating > 8 && rating <= 9 ){
+        return 'background: linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+      }
+
+      if(rating > 9){
+        return 'background: linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+      }
     }
 
 
@@ -157,5 +153,5 @@
   
   }
   const app = new BooksList();
-  app.init();
-} 
+  
+}
